d039e886366683e0eb025d30bf8ced7364a1d4d7
Aurélien Malisart <aurelien.malisart@gmail.com>
2015-07-31 16:36:58 +0200

    form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def age_options
+    (0..110).to_a.collect do |age|
+      [age, age]
+    end
+  end
+
+  def gender_options
+    [
+      ['Male',   'M'],
+      ['Female', 'F'],
+    ]
+  end
 end
