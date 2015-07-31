505a86dcf2d5b6d4247ed15efb94c57967585b54
Aurélien Malisart <aurelien.malisart@gmail.com>
2015-07-31 15:09:23 +0200

    body

--- app/controllers/api/twilio/messages_controller.rb
+++ app/controllers/api/twilio/messages_controller.rb
@@ -1,9 +1,10 @@
 class Api::Twilio::MessagesController < ActionController::Base
   def create
     sender = params[:From]
+    body   = params[:Body]
 
     response = Twilio::TwiML::Response.new do |r|
-      r.Message "You will die at 42 years old in a plane."
+      r.Message "You will die at 42 years old in a plane. #{body}"
     end
 
     render :text => response.text, :layout => false
