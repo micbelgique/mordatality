9e05e00c2e80c89fed3fa9f8bc7c5479cbf03811
Aurélien Malisart <aurelien.malisart@gmail.com>
2015-08-02 11:41:58 +0200

    dsign

--- app/controllers/estimations_controller.rb
+++ app/controllers/estimations_controller.rb
@@ -2,14 +2,18 @@ class EstimationsController < ApplicationController
   def create
     sleep 3
 
-    city        = City.find(params[:city])
-    province_id = Province.find_by_zip_code(city.zip)
-    date_of_birth = Date.parse(params[:date_of_birth], "%Y-%m-%d")
+    begin
+      city        = City.find(params[:city])
+      province_id = Province.find_by_zip_code(city.zip)
+      date_of_birth = Date.parse(params[:date_of_birth], "%Y-%m-%d")
 
-    @estimation = EstimationService.new(
-      date_of_birth,
-      params[:gender],
-      province_id
-    ).estimate
+      @estimation = EstimationService.new(
+        date_of_birth,
+        params[:gender],
+        province_id
+      ).estimate
+    rescue
+      @error = "Données invalides."
+    end
   end
 end
